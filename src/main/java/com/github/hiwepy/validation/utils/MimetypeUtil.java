e8791ec0f86fe69dd9220e7b6a725dfb9c40f57a
wandl <hiwepy@163.com>
2026-05-27 21:55:35 +0800

    chore: migrate groupId from com.github.hiwepy to io.github.hiwepy

--- src/main/java/com/github/hiwepy/validation/utils/MimetypeUtil.java
+++ src/main/java/com/github/hiwepy/validation/utils/MimetypeUtil.java
@@ -1,4 +1,4 @@
-package com.github.hiwepy.validation.utils;
+package io.github.hiwepy.validation.utils;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
